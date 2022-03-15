90fc64de0ab3d1ff5c9fcfda4d42a134874e722d
Ricardo Crossley <richi.escamilla@gmail.com>
2022-03-14 23:21:44 -0700

    messed up on editor.js script, pushing better script, waiting to redeploy

--- Main/client/src/js/editor.js
+++ Main/client/src/js/editor.js
@@ -21,7 +21,7 @@ export default class {
       tabSize: 2,
     });
     
-    here is the get route ... where is put
+    // here is the get route ... where is put
     // When the editor is ready, set the value to whatever is store in indexeddb
     getDb().then((data) => {
       console.info('Loaded data from IndexedDB, injecting into editor');
